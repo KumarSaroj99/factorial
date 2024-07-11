1f2ed915e0bea797e66131fd80af41cea20a1188
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-07-11 23:53:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, factorial = 1;
+            Console.WriteLine("Enter the number :");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            for (int i = number1; i > 0; i--)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("Factorial of " + number1 + " is " + factorial);
+            Console.ReadLine();
+        }
+    }
+
+}
